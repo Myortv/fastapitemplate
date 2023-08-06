7addaa8a8e92ff3ba4e762df43ede3be915f6f53
Myortv <93913240+Myortv@users.noreply.github.com>
2023-08-06 23:22:01 +0300

    adderd opened and closed routers

--- app/api/v1/rename.py
+++ app/api/v1/rename.py
@@ -15,10 +15,22 @@ from app.utils.dep import get_token_content
 router = APIRouter()
 
 
-@router.get('/', response_model=RenameInDB)
+@router.get('/closed', response_model=RenameInDB)
 async def get_pg_user_by_name(
     name: str,
     token=Depends(get_token_content),
+):
+    """ return user object from pg_users postgres table """
+    if result := await rename_controller.get_by_name(name):
+        return result
+    else:
+        # make sure to raise excepions in router if possible
+        raise HTTPException(404)
+
+
+@router.get('/', response_model=RenameInDB)
+async def get_pg_user_by_name(
+    name: str,
 ):
     """ return user object from pg_users postgres table """
     if result := await rename_controller.get_by_name(name):
